2d4c6006406cf1da48020c2ddef611b0bfa185fe
EwertonBello <ewerton.bello1212@gmail.com>
2019-07-07 13:04:45 -0300

    Updates

---
+++ precache-manifest.1de323b60806ca121ec3be0c5abfc49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7f5d53daefe864c4b705d3f90c7e17",
+    "url": "/deploy-reactjs/index.html"
+  },
+  {
+    "revision": "51ffde14e8aa615645da",
+    "url": "/deploy-reactjs/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/deploy-reactjs/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "51ffde14e8aa615645da",
+    "url": "/deploy-reactjs/static/js/main.c6243033.chunk.js"
+  },
+  {
+    "revision": "8068d9167046fe7d3ec7",
+    "url": "/deploy-reactjs/static/js/runtime~main.9405b2e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
